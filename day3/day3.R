fac7bcbc550b6749c59f024a04a275d3d875d56c
Brendan Matthew Galdo <brendan.m.galdo@gmail.com>
2024-12-03 23:35:28 -0500

    solution to part 2

--- day3/day3.R
+++ day3/day3.R
@@ -1,75 +1,68 @@
-library(stringr)
 setwd("~/Documents/GitHub/advent_of_code_2024/day3")
-input <- readr::read_lines("input.txt") |> 
-  paste(collapse = "") |> unlist() |> as.character()
+library(stringr)
+library(dplyr)
+library(tidyr)
+# PART 1 **BEGIN**
+input <- 
+  readr::read_lines("input.txt") |> 
+  paste(collapse = "") |> 
+  unlist() |> 
+  as.character()
 
 nchar <- nchar(input)
 
-substrings <- str_extract_all(input, 
-                              regex("([m][u][l][(]\\d{1,3},\\d{1,3}[)])")) |> 
+substrings <- 
+  str_extract_all(input,regex("([m][u][l][(]\\d{1,3},\\d{1,3}[)])")) |> 
   unlist() 
-# part 1
+
+# PART 1 ***solution***
 sapply(substrings,\(x)
        str_extract_all(x,regex("\\d{1,3}")) |> 
-         unlist() |> 
+         unlist() |>  
          as.numeric() |> 
          prod()) |> unlist() |> sum()
 
+# PART 2 **BEGIN**
+mul_idx = 
+  gregexpr(pattern="([m][u][l][(]\\d{1,3},\\d{1,3}[)])",text=input) |> 
+  unlist()
+dos_idx = 
+  gregexpr(pattern="([d][o][(][)])",text=input) |> 
+  unlist()
+dont_idx = 
+  gregexpr(pattern="([d][o][n]['][t])",text=input) |> 
+  unlist()
 
-# <- sapply(1:nchar,function(x)substr(x,x))
-# input |> substr(,1:100,1:100)
-# str_split
+n_muls = length(mul_idx) 
+add_mull = numeric(n_muls) 
 
-# substring1 <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-# substring2 <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d\\d[)])"))
-# substring3 <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d\\d\\d[)])"))
-# 
-# substring4 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d,\\d[)])"))
-# substring5 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d,\\d\\d[)])"))
-# substring6 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d,\\d\\d\\d[)])"))
-# 
-# substring7 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d\\d,\\d[)])"))
-# substring8 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d\\d,\\d\\d[)])"))
-# 
-# substring9 <- str_extract_all(input, regex("([m][u][l][(]\\d\\d\\d,\\d\\d\\d[)])"))
-# 
-# substring9 <- str_extract_all(input, regex("mul$"))
-# 
-# substring9[[1]] |> str_extract_all(regex('(\\d+)'))
-# 
+# order do and don't idxs
+do_df = rbind(data.frame(idx=dos_idx,do=1),data.frame(idx=dont_idx,do=0)) 
+do_df = do_df[order(do_df$idx),] 
+do_df = dplyr::left_join(data.frame(idx = 1:max(mul_idx)),do_df)
 
+# fill in idices where the do state is valid
+do_df$do[1] <- 1
+do_df = do_df |> 
+  tidyr::fill(do,
+              .direction = "down") 
 
+# find the subset of indices where do is valid
+mul_df = data.frame(idx = mul_idx,
+                    which_mul = 1:length(mul_idx)) |> 
+  left_join(do_df) |> 
+  filter(do==TRUE)
 
+# now repeat part 1
+substrings <- str_extract_all(input, 
+  regex("([m][u][l][(]\\d{1,3},\\d{1,3}[)])")) |> 
+  unlist() 
 
-
-
-
-
-
-
-
-
-substring <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-substring <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-substring <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-substring <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-substring <- str_extract_all(input, regex("([m][u][l][(]\\d,\\d[)])"))
-
-regexpr(pattern = "([m][u][l][(]\\d,\\d[)])",
-        text = test,perl=TRUE)
-
-# grep(pattern='([m][u][l][(]\\\d)',test)
-# 
-# grep(pattern='([m][u][l][(])',test)
-regexpr(pattern = "([m][u][l][(]\\d,\\d[)])",text = input[1])
-regexpr(pattern = "([m][u][l][(]\\d,\\d\\d[)])",text = input[1])
-regexpr(pattern = "([m][u][l][(]\\d,\\d\\d\\d[)])",text = input[1])
-
-grep(pattern = "([m][u][l][(]\\d\\d,\\d[)])",x = test)
-grep(pattern = "([m][u][l][(]\\d\\d,\\d\\d[)])",x = test)
-grep(pattern = "([m][u][l][(]\\d\\d,\\d\\d\\d[)])",x = test)
-
-reg.finalizer(pattern = "([m][u][l][(]\\d\\d\\d,\\d[)])",x = test)
-grep(pattern = "([m][u][l][(]\\d\\d\\d,\\d\\d[)])",x = test)
-grep(pattern = "([m][u][l][(]\\d\\d\\d,\\d\\d\\d[)])",x = test)
-
+# PART 2 ***solution***
+sapply(substrings[mul_df$which_mul],\(x)
+       str_extract_all(x,regex("\\d{1,3}")) |> 
+         unlist() |> 
+         as.numeric() |> 
+         prod()) |> 
+  unlist() |> 
+  sum()
